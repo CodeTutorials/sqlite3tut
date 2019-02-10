6464ab11ef74ff748dedbc6cef05d59ea0ead906
CodeTutorials <47407850+CodeTutorials@users.noreply.github.com>
2019-02-10 15:03:29 +0200

    Add files via upload

--- editcategory.php
+++ editcategory.php
@@ -7,7 +7,7 @@ if(!isset($_POST['submit'] ) ) {
 $id = $_REQUEST['id'];
 $sq = $db->query("SELECT * FROM category WHERE id = '$id' ");
 while ($row = $sq->fetchArray(SQLITE3_ASSOC) ) { 
-echo "<b>" . $row[cat_title] . "</b><br>";
+echo "<b>" . $row['cat_title'] . "</b><br>";
     echo "<form action='$_SERVER[PHP_SELF]' method='post'>
     <label>Enter Category</label><br><input type='text' value='$row[cat_title]' name='cat' /><br><input type='text' value='$row[id]' name='id' />
     <input type='submit' name='submit' value='submit' /></form>"; } 
@@ -15,7 +15,7 @@ echo "<b>" . $row[cat_title] . "</b><br>";
 $cat = cl($_POST['cat']);
 $id = $_POST['id'];
 echo "$cat updated in category table<br>";
-echo "<a href='updatecategorylead.php'>Return and edit another</a><br><a href='index.php'>Return Home</a>";
+echo "<a href='updatecategorylead.php'>Return and edit another</a><br>";
 $db->exec("UPDATE category SET cat_title = '$cat' WHERE id = '$id'");
                      
 }
--- updatecategorylead.php
+++ updatecategorylead.php
@@ -2,5 +2,5 @@
 require_once "configure.php";
 $sq = $db->query("SELECT * FROM category");
 while ($row = $sq->fetchArray(SQLITE3_ASSOC) ) { 
-echo "<a href='editcategory.php?id=$row[id]'><b> Edit " . $row[cat_title] . "</b></a>|<a href='deletecategory.php?id=$row[id]'><b> Delete " . $row[cat_title] . "</b></a><br>"; } 
+echo "<a href='editcategory.php?id=$row[id]'><b> Edit " . $row['cat_title'] . "</b></a>|<a href='deletecategory.php?id=$row[id]'><b> Delete " . $row['cat_title'] . "</b></a><br>"; } 
 ?>
